e3d0d4b6eae11c2217501aa06d87d23a1244cbff
red4stone <creeperinthegarden@gmail.com>
2020-03-08 10:38:06 -0400

    Added RavenBlinkin and method stubs
    Method stubs so Blinkin stuff can get going.

---
+++ InfiniteRecharge/src/main/java/frc/ravenhardware/RavenBlinkin.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.ravenhardware;
+
+/**
+ * "ButtonCode" but for colors of LEDs, mixed with acutal methods for setting the color.
+ */
+public class RavenBlinkin {
+
+// Dictionary of color codes
+
+
+
+
+// Color methods
+
+    public void blinkGreen() {
+
+    }
+
+    public void solidGreen() {
+
+    }
+
+    public void blinkYellow() {
+
+    }
+
+    public void solidYellow() {
+
+    }
+
+    public void blinkRed() {
+
+    }
+
+    public void solidRed() {
+
+    }
+
+    public void blinkBlue() {
+
+    }
+
+    public void solidBlue() {
+
+    }
+
+    public void solidOff() {
+
+    }
+}
